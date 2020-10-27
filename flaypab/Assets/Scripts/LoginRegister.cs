cc51edbbbfd13705b5103e5a28337836929310b1
Devin Hicks <devinhicks.az@gmail.com>
2020-10-27 11:08:34 -0700

    authentication

---
+++ flaypab/Assets/Scripts/LoginRegister.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using PlayFab;
+using PlayFab.ClientModels;
+using TMPro;
+
+public class LoginRegister : MonoBehaviour
+{
+    public TMP_InputField usernameInput;
+    public TMP_InputField passwordInput;
+
+    public TextMeshProUGUI displayText;
+
+    public UnityEvent onLoggedIn;
+
+    [HideInInspector]
+    public string playFabId;
+
+    // instance
+    public static LoginRegister instance;
+    public void Awake()
+    {
+        instance = this;
+    }
+
+    public void OnRegister()
+    {
+        RegisterPlayFabUserRequest registerRequest = new RegisterPlayFabUserRequest
+        {
+            Username = usernameInput.text,
+            DisplayName = usernameInput.text,
+            Password = passwordInput.text,
+            RequireBothUsernameAndEmail = false
+        };
+
+        PlayFabClientAPI.RegisterPlayFabUser(registerRequest,
+            result =>
+            {
+                SetDisplayText(result.PlayFabId, Color.green);
+            },
+            error =>
+            {
+                SetDisplayText(error.ErrorMessage, Color.red);
+            }
+        );
+    }
+
+    public void OnLoginButton()
+    {
+        LoginWithPlayFabRequest loginRequest = new LoginWithPlayFabRequest
+        {
+            Username = usernameInput.text,
+            Password = passwordInput.text
+        };
+
+        PlayFabClientAPI.LoginWithPlayFab(loginRequest,
+            result =>
+            {
+                SetDisplayText("Logged in as: " + result.PlayFabId, Color.green);
+
+                playFabId = result.PlayFabId;
+
+                if (onLoggedIn != null)
+                    onLoggedIn.Invoke();
+            },
+            error => SetDisplayText(error.ErrorMessage, Color.red)
+        );
+    }
+
+   void SetDisplayText(string text, Color color)
+    {
+        displayText.text = text;
+        displayText.color = color;
+    }
+}
